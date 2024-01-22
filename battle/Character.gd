84da247a9b5769bb4f8e48b87db7aaf7ac0cd532
Endermandels <elijahdelavar@gmail.com>
2024-01-22 13:10:45 -0800

    Limit player's movement to player's mov stat

--- battle/Character.gd
+++ battle/Character.gd
@@ -7,7 +7,6 @@ var grid_rows = 5
 var grid_cols = 7
 
 var grid_pos = [3, 4]
-var possible_moves = []
 
 """
 VERY USEFUL INFORMATION BELOW:
@@ -15,7 +14,7 @@ VERY USEFUL INFORMATION BELOW:
 """
 
 func _ready():
-	possible_moves = update_possible_moves(grid_pos, stats.mov)
+	pass
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -25,39 +24,43 @@ func _input(event):
 			# Player sprite is clicked
 			print("Player sprite clicked!")
 
-func update_possible_moves(start, moves_left):
+func move_to(start, target, moves_left, moved):
 	if moves_left == 0:
-		return [start]
+		return false
 	
-	var moves = []
-	
-	for col_offset in [-1, 0, 1]:
+	for col_offset in [-1, 1]:
 		var adj_col = start[0] + col_offset
 		var adj_row = start[1]
 		
 		# Check if the adjacent square is within the grid
 		if adj_col >= 0 and adj_col < grid_cols and adj_row >= 0 and adj_row < grid_rows:
-			for space in update_possible_moves([adj_col, adj_row], moves_left-1):
-				if not space in moves:
-					moves.append(space)
+			var new_space = [adj_col, adj_row]
+			# Update moves left 
+			if target == new_space:
+				stats.move(moved)
+				return true
+			# Check adjacent squares
+			if moves_left > 1 and move_to(new_space, target, moves_left-1, moved + 1):
+				return true
 	
-	for row_offset in [-1, 0, 1]:
+	for row_offset in [-1, 1]:
 		var adj_col = start[0]
 		var adj_row = start[1] + row_offset
 		
 		# Check if the adjacent square is within the grid
 		if adj_col >= 0 and adj_col < grid_cols and adj_row >= 0 and adj_row < grid_rows:
-			for space in update_possible_moves([adj_col, adj_row], moves_left-1):
-				if not space in moves:
-					moves.append(space)
+			var new_space = [adj_col, adj_row]
+			# Update moves left 
+			if target == new_space:
+				stats.move(moved)
+				return true
+			# Check adjacent squares
+			if moves_left > 1 and move_to(new_space, target, moves_left-1, moved + 1):
+				return true
 	
-	return moves
-
-func can_move_to_grid_pos(new_grid_pos):
-	return new_grid_pos in possible_moves
+	return false
 
 func _on_Space_move_player(new_position, new_grid_pos):
-	if can_move_to_grid_pos(new_grid_pos):
+	if move_to(grid_pos, new_grid_pos, stats.mov_left, 1):
 		sprite.global_position = new_position
 		grid_pos = new_grid_pos
-		possible_moves = update_possible_moves(grid_pos, stats.mov)
