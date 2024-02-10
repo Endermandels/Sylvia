a2677e2c19e50a37ab59687c0e6b7b71f77bf280
Emma Eccles <eccles.emma@gmail.com>
2024-02-10 11:03:56 -0800

    Add signals for options menu font settings
    Adds signals for accessibility toggle settings

--- start_menu/OptionsMenu.gd
+++ start_menu/OptionsMenu.gd
@@ -2,3 +2,17 @@ extends Control
 
 func _on_return_pressed():
 	get_tree().change_scene_to_file('res://start_menu/start_menu.tscn')
+
+
+func _on_dyslexia_friendly__font_toggled(toggled_on):
+	if(toggled_on == true):
+		print("dyslexia friendly font on")
+	else:
+		print("default font on")
+
+
+func _on_large_text_toggled(toggled_on):
+	if(toggled_on == true):
+		print("large text on")
+	else:
+		print("default text size on")
